0faecb9e16079870364acc82c8ced30bb807c4a7
Carlos Alberto Filho <calbertofilho@gmail.com>
2023-06-27 19:58:44 -0300

    Update 27-06-2023

---
+++ main.py
@@ -0,0 +1,63 @@
+'''
+Jogo Tetris em Python
+Utilizando a biblioteca: PyGame
+
+Criado por: Carlos Alberto Morais Moura Filho
+Versão: 1.0
+Atualizado em: 27/06/2022
+'''
+# pylint: disable=no-member
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-statements
+# pylint: disable=c-extension-no-member
+# pylint: disable=no-name-in-module
+
+# Bibliotecas
+import pygame
+from os import environ, path
+from sys import exc_info as info, exit, platform
+
+
+def close_game():
+    '''Função que encerra todas as bibliotecas e fecha o jogo'''
+    pygame.display.quit()
+    pygame.mixer.quit()
+    pygame.quit()
+    exit()
+
+def main():
+    '''Função principal que trata de toda a execução do jogo'''
+    # Centraliza a janela do jogo no monitor
+    environ['SDL_VIDEO_CENTERED'] = '1'
+    pygame.init()
+    pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
+    # Criação da janela
+
+
+try:
+    if __name__ == "__main__":
+        while True:
+            main()
+except SyntaxError as syntax_exception:
+    print(f'Oops! Ocorreu um erro de sintaxe no código.\n\
+        __class__ = {syntax_exception.__class__}\n\
+        __doc__ = {syntax_exception.__doc__}\n\
+        args = {syntax_exception.args}')
+except (ValueError, ZeroDivisionError) as value_exception:
+    print(f'Oops! Ocorreu um erro de valores.\n\
+        __class__ = {value_exception.__class__}\n\
+        __doc__ = {value_exception.__doc__}\n\
+        args = {value_exception.args}')
+except TypeError as type_exception:
+    print(f'Oops! Ocorreu um erro de conversão de tipo de dados.\n\
+        __class__ = {type_exception.__class__}\n\
+        __doc__ = {type_exception.__doc__}\n\
+        args = {type_exception.args}')
+except Exception as general_exception:
+    print(f'Oops! Ocorreu um erro não identificado.\n\
+        __class__ = {general_exception.__class__}\n\
+        __doc__ = {general_exception.__doc__}\n\
+        args = {general_exception.args}')
+finally:
+    close_game()
